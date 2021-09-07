3e1670055064cb9b1d8d57ddc49dadf46da21d03
= <gwylim@gtservices.be>
2021-09-07 12:40:18 +0000

    fix indentation error

--- birthday/models/models.py
+++ birthday/models/models.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
- from odoo import models, fields, api
+from odoo import models, fields, api
 
 
- class event_gt_extended(models.Model):
-     _inherit = 'res.partner'
-        
+class event_gt_extended(models.Model):
+    _inherit = 'res.partner'
+    
     birthday = fields.Datetime('Date of birth')
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100

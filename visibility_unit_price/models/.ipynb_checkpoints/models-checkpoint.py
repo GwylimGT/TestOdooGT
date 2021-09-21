e04cae32a7126cf7018066a8f02589c8c1df0722
GwylimGT <gwylim@gtservices.be>
2021-09-21 09:37:49 +0000

    edit module unit price visibility

--- visibility_unit_price/models/.ipynb_checkpoints/models-checkpoint.py
+++ visibility_unit_price/models/.ipynb_checkpoints/models-checkpoint.py
@@ -4,6 +4,6 @@ from odoo import models, fields, api
 
 
 class sales_order_checkbox_show_unit_price(models.Model):
-    _inherit = 'sales.order'
+    _inherit = 'sale.order'
 
     show_unit_price_to_client = fields.Boolean(string="Show Unit Price to client", required=True, default=True)

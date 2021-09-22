3f762bb7cd1c3f5ce7263130711fecb3555293ff
GwylimGT <gwylim@gtservices.be>
2021-09-22 14:06:16 +0000

    another extra field test - for production update test

--- visibility_unit_price/models/.ipynb_checkpoints/models-checkpoint.py
+++ visibility_unit_price/models/.ipynb_checkpoints/models-checkpoint.py
@@ -7,3 +7,4 @@ class sales_order_checkbox_show_unit_price(models.Model):
     _inherit = 'sale.order.line'
 
     show_unit_price_to_client = fields.Boolean(string="Show Unit Price to client", required=True, default=True)
+    extra_field_test = fields.Boolean(string="Extra test", required=True, default=True)
